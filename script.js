c89234ed3429202faa7bf8a752354a760c1b33d6
Linda Zhao <lindajzhao@gmail.com>
2018-01-30 17:15:49 -0500

    done mobile menu + js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(function () {
+    // mobile nav slide out
+    $(".js--hamburger").on("click", function() {
+
+        if (window.matchMedia('(max-width: 700px)').matches) {
+            $(".nav__main").toggleClass("open");
+            console.log("clicked");
+        }
+    });
+
+});
