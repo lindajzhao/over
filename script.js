8c74c063951c650d75d5d0ce51e46983029d31ba
Linda Zhao <lindajzhao@gmail.com>
2018-01-31 10:37:08 -0500

    mobile nav

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 $(function () {
     // mobile nav slide out
     $(".js--hamburger").on("click", function() {
-
         if (window.matchMedia('(max-width: 700px)').matches) {
             $(".nav__main").toggleClass("open");
-            console.log("clicked");
         }
     });
+
+    // $(".js--hamburger").on("click", function () {
+    //     // if (window.matchMedia('(max-width: 700px)').matches) {
+    //         $(".nav__main").toggleClass("open");
+    //     // }
+    // });
+    // if (window.matchMedia('(min-width: 699px)').matches) {
+    //     $(".nav__main").removeClass("open");
+    //     console.log("remove open");
+    // }
 
 });
