65e307ccb8d6e0e88bbaf0e6ab07f000e7eac945
Linda Zhao <lindajzhao@gmail.com>
2018-01-31 12:41:35 -0500

    fixed: broken search icon, footer bar, quote spacing

--- script.js
+++ script.js
@@ -6,15 +6,4 @@ $(function () {
             $(".nav__main").toggleClass("open");
         }
     });
-
-    // $(".js--hamburger").on("click", function () {
-    //     // if (window.matchMedia('(max-width: 700px)').matches) {
-    //         $(".nav__main").toggleClass("open");
-    //     // }
-    // });
-    // if (window.matchMedia('(min-width: 699px)').matches) {
-    //     $(".nav__main").removeClass("open");
-    //     console.log("remove open");
-    // }
-
 });
